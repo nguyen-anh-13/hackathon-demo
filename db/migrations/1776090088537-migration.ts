d602ad761454076228509d85a1dc1f7ab8eac69b
nguyen-anh-13 <anh.nguyen@netko-solution.com>
2026-04-15 15:09:57 +0700

    fix migration

--- db/migrations/1776090088537-migration.ts
+++ db/migrations/1776090088537-migration.ts
@@ -5,10 +5,23 @@ export class Migration1776090088537 implements MigrationInterface {
     name = 'Migration1776090088537'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "userId" integer NOT NULL, "username" character varying(255) NOT NULL, "name" character varying(255) NOT NULL, "state" character varying(50) NOT NULL DEFAULT 'active', "password" character varying(255) NOT NULL, CONSTRAINT "UQ_8bf09ba754322ab9c22a215c919" UNIQUE ("userId"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" (
+            "id" SERIAL NOT NULL, 
+            "created_at" TIMESTAMP NOT NULL DEFAULT now(), 
+            "updated_at" TIMESTAMP NOT NULL DEFAULT now(), 
+            "userId" integer NOT NULL, 
+            "username" character varying(255) NOT NULL, 
+            "name" character varying(255) NOT NULL, 
+            "email" character varying(255), -- Thêm dòng này
+            "state" character varying(50) NOT NULL DEFAULT 'active', 
+            "password" character varying(255) NOT NULL, 
+            CONSTRAINT "UQ_8bf09ba754322ab9c22a215c919" UNIQUE ("userId"), 
+            CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+        )`);
 
         const password77 = hashSync("nguyen.nguyen", 10);
         const password76 = hashSync("dai.nguyen", 10);
+        const password25 = hashSync("duongnt", 10);
 
         await queryRunner.manager
             .createQueryBuilder()
@@ -25,7 +38,8 @@ export class Migration1776090088537 implements MigrationInterface {
                 { userId: 80, username: "hai.phan", name: "Phan Hai", state: "active", password: "" },
                 { userId: 91, username: "ha.nguyen", name: "ha.nguyen", state: "active", password: "" },
                 { userId: 92, username: "quynh.anh", name: "quynh.anh", state: "active", password: "" },
-                { userId: 90, username: "kien.nguyen", name: "kien.nguyen", state: "active", password: "" }
+                { userId: 90, username: "kien.nguyen", name: "kien.nguyen", state: "active", password: "" },
+                { userId: 25, username: "duongnt", name: "Tùng Dương", state: "active", password: password25, email: 'duongnt@netko-solution.com' },
             ])
             .execute();
     }
--- db/migrations/1776228601209-migration.ts
+++ db/migrations/1776228601209-migration.ts
@@ -1,29 +1,13 @@
-import { hashSync } from "bcrypt";
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migration1776228601209 implements MigrationInterface {
     name = 'Migration1776228601209'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const password25 = hashSync("duongnt", 10);
-
         await queryRunner.query(`ALTER TABLE "issues" ADD "is_sent" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(
-            `ALTER TABLE "users" ADD "email" character varying(255) NOT NULL DEFAULT ''`
-        );
-
-        await queryRunner.query(
-            `INSERT INTO "users" ("created_at", "updated_at", "userId", username, name, state, password, "email")
-             VALUES (NOW(), NOW(), $1, $2, $3, $4, $5, $6)`,
-            [25, 'duongnt', 'Tùng Dương', 'active', password25, 'duongnt@netko-solution.com']
-        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "email"`);
         await queryRunner.query(`ALTER TABLE "issues" DROP COLUMN "is_sent"`);
-
-        await queryRunner.query(`DELETE FROM "users" WHERE "userId" = 25`);
     }
-
 }
